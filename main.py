e921f0774379fd73656a214976a1f98ee44e97be
Adam Monawir <adammonawir@hotmail.com>
2024-01-15 12:46:50 -0800

    start of movement

--- main.py
+++ main.py
@@ -42,6 +42,45 @@ class Planet:
         y = self.y * self.SCALE + HEIGHT / 2
         pygame.draw.circle(win, self.colour, (x, y), self.radius)
 
+    #Calculating attraction
+    def attraction(self, other):
+        other_x, other_y = other.x, other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x**2 + distance_y**2)
+
+        #checking if sun
+        if other.sun:
+            self.distance_to_sun = distance
+        
+        #Calculating forces
+        force = self.G * self.mass * other.mass / distance**2
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+        return force_x, force_y
+    
+    #Calculating and moving by velocity
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+            
+            #force calculation
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        #Velocity calculation
+        self.x_vel += total_fx / self.mass * self.TIMESTEP
+        self.y_vel += total_fy / self.mass * self.TIMESTEP
+
+        #Placement Calculations
+        self.x += self.x_vel * self.TIMESTEP
+        self.y += self.y_vel * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
+
 #Opens Window
 def main():
     run = True
@@ -71,8 +110,11 @@ def main():
         
         #Will draw planets
         for planet in planets:
+            planet.update_position(planets)
             planet.draw(WIN)
-        
+
         pygame.display.update()
     pygame.quit()
+
+
 main()
