9ed3018031ce07313fb524461086433b5ad301e4
Adam Monawir <adammonawir@hotmail.com>
2024-01-15 10:27:36 -0800

    Window setup

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("F = ma")
+import pygame 
+import math
+pygame.init()
+
+#Window Properties
+WIDTH, HEIGHT = 800, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Solar System")
+
+#Opens Window
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+main()
