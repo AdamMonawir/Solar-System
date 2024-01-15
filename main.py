4b0cf874263f3dc57aa255748d4a4b50e6d1317f
Adam Monawir <adammonawir@hotmail.com>
2024-01-15 11:27:23 -0800

    changed window scale

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import math
 pygame.init()
 
 #Window Properties
-WIDTH, HEIGHT = (800, 800)
+WIDTH, HEIGHT = (1000, 800)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Solar System")
 #Colours
