4f061c6d5b731a2005adbcd27972e6feb636e133
Adam Monawir <adammonawir@hotmail.com>
2024-01-15 11:15:12 -0800

    Sun made + drawn section setup

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ pygame.init()
 WIDTH, HEIGHT = (800, 800)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Solar System")
+#Colours
 WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
 
 #Planet definitions
 class Planet:
@@ -15,6 +17,7 @@ class Planet:
     SCALE = 250 / AU  # 1AU = 100pixels
     TIMESTEP = 3600 * 24 # 1 day
 
+    #Variables for each planet
     def __init__(self, x, y, radius, colour, mass):
         self.x = x
         self.y = y
@@ -29,20 +32,39 @@ class Planet:
         self.x_vel = 0
         self.y_vel = 0
 
+    #Draw the planets
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(win, self.colour, (x, y), self.radius)
+
 #Opens Window
 def main():
     run = True
     #to be defined for loop
     clock = pygame.time.Clock()
 
+    #Making Planets
+    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
+    sun.sun = True
+
+    #planets to be drawn
+    planets = [sun]
+
     while run:
         #updates per second
         clock.tick(60)
         #WIN.fill(WHITE)
-        #pygame.display.update()
 
+        #Will close when x is pressed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
+        #Will draw planets
+        for planet in planets:
+            planet.draw(WIN)
+        
+        pygame.display.update()
     pygame.quit()
 main()
